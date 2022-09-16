d73e0d40a527e04e4c3695deee498081685c9920
sshettyn <ShettyS_P@boqdev.com.au>
2022-09-16 10:50:03 +0530

    added commit

--- app/src/main/java/com/deloitte/retrofitproject/view/HomeActivity.kt
+++ app/src/main/java/com/deloitte/retrofitproject/view/HomeActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.deloitte.retrofitproject.R
+import com.deloitte.retrofitproject.repository.HomeActivityRepository
 import com.deloitte.retrofitproject.viewmodel.HomeViewModel
 
 class HomeActivity : AppCompatActivity() {
@@ -21,10 +22,10 @@ class HomeActivity : AppCompatActivity() {
     setContentView(R.layout.activity_home)
 
     homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    homeViewModel.setHomeRepository(HomeActivityRepository())
     val progressBar: ProgressBar = findViewById(R.id.progressBar)
     progressBar.visibility = View.VISIBLE
-    homeViewModel.getUser()!!.observe(this, Observer { albums ->
+    homeViewModel.getAlbums()!!.observe(this, Observer { albums ->
       progressBar.visibility = View.GONE
       val recyclerView: RecyclerView = findViewById(R.id.recycler)
       val albumAdapter = AlbumAdapter(albums)
